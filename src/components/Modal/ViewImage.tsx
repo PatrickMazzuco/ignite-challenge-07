87eb15ee2ac30a950f0aa53afde0b3680bbe108a
Patrick Mazzuco <patrick.flores@acad.pucrs.br>
2021-07-30 23:14:38 -0300

    feat: update card modal

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,25 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="5xl">
+      <ModalOverlay />
+      <ModalContent maxWidth={900} maxHeight={600} w="auto" h="auto" mx="auto">
+        <ModalBody p={0}>
+          <Image src={imgUrl} maxWidth={900} maxHeight={600} />
+        </ModalBody>
+        <ModalFooter
+          justifyContent="flex-start"
+          py="2"
+          px="2.5"
+          borderBottomRadius={6}
+          bg="pGray.800"
+        >
+          <Link href={imgUrl} isExternal fontSize={14}>
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
